86514686c7203b17f06a77199435cf97fc0d2efd
Mattias-Blom-Johanneberg <mattias.blom@elev.ga.ntig.se>
2023-02-28 11:23:27 +0100

    Kodat alllt.

--- app.rb
+++ app.rb
@@ -2,17 +2,64 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
+require 'bcrypt'
+
+enable :sessions
 
 get('/')  do
     slim(:start)
 end 
 
+get('/login') do
+    slim(:login)
+end
+
+get('/register') do
+    slim(:register)
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/watch_database.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+
+    if BCrypt::Password.new(pwdigest) == password
+        session[:id] = id
+        redirect('/watches/favourites')
+    else
+        "Fel lösenord!!!!!!?"
+    end
+end
+
 get('/watches/favourites') do
-    slim(:"watches/favourites")
+    id = session[:id].to_i
+    db = SQLite3::Database.new('db/watch_database.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM user_favourites WHERE user_id = ?", id)
+    slim(:"watches/favourites", locals:{favourites:result})
+end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+
+    if (password == password_confirm)
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new('db/watch_database.db')
+        db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)", username, password_digest)
+        redirect('/')
+    else 
+        "Lösenordet matchade inte!"
+    end
 end
 
 get('/watches') do
-    db = SQLite3::Database.new("db/watch_database.db")
+    db = SQLite3::Database.new('db/watch_database.db')
     db.results_as_hash = true
     result = db.execute("SELECT watch_id, watch_name FROM watches")
     p result
