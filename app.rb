0cec456def738a8a70c9fab129583d3b433d8f0d
Mattias-Blom-Johanneberg <mattias.blom@elev.ga.ntig.se>
2023-03-07 11:58:34 +0100

    Kodat under lektion hemma

--- app.rb
+++ app.rb
@@ -25,22 +25,23 @@ post('/login') do
     db.results_as_hash = true
     result = db.execute("SELECT * FROM users WHERE username = ?", username).first
     pwdigest = result["pwdigest"]
-    id = result["id"]
+    user_id = result["user_id"]
 
     if BCrypt::Password.new(pwdigest) == password
-        session[:id] = id
+        session[:id] = user_id
+        p session[:id]
         redirect('/watches/favourites')
     else
-        "Fel lösenord!!!!!!?"
+        "Fel lösenord!"
     end
 end
 
 get('/watches/favourites') do
-    id = session[:id].to_i
+    user_id = session[:id].to_i
     db = SQLite3::Database.new('db/watch_database.db')
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM user_favourites WHERE user_id = ?", id)
-    slim(:"watches/favourites", locals:{favourites:result})
+    result = db.execute("SELECT *, watch_name FROM user_favourites JOIN watches ON user_favourites.watch_id = watches.watch_id WHERE user_id = ?", user_id)
+    slim(:"/watches/favourites", locals:{favourites:result})
 end
 
 post('/users/new') do
@@ -63,11 +64,11 @@ get('/watches') do
     db.results_as_hash = true
     result = db.execute("SELECT watch_id, watch_name FROM watches")
     p result
-    slim(:"watches/index",locals:{watches:result})
+    slim(:"/watches/index",locals:{watches:result})
 end
 
 get('/watches/new') do
-    slim(:"watches/new")
+    slim(:"/watches/new")
 end
 
 post('/watches/new') do
@@ -79,7 +80,7 @@ post('/watches/new') do
     db = SQLite3::Database.new("db/watch_database.db")
     brand_id = db.execute("SELECT brand_id FROM watch_brands WHERE brand_name = ?", brand_name).first
     db.execute("INSERT INTO watches (watch_name, brand_name, brand_id, content, movement) VALUES (?,?,?,?,?)", watch_name, brand_name, brand_id, content, movement)
-    redirect(:watches)
+    redirect('/watches')
 end
 
 post('/watches/:id/delete') do
@@ -102,8 +103,39 @@ get('/watches/:id/edit') do
     id = params[:id].to_i
     db = SQLite3::Database.new("db/watch_database.db")
     db.results_as_hash = true
-    result = db.execute("SELECT * FROM watches WHERE watch_id =?", id).first
+    result = db.execute("SELECT * FROM watches WHERE watch_id = ?", id).first
     slim(:"/watches/edit", locals:{result:result})
+end
+
+get('/watches/:id/like') do
+    watch_id = params[:id].to_i
+    user_id = session[:id]
+    db = SQLite3::Database.new("db/watch_database.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT watch_id FROM user_favourites WHERE user_id = ?", user_id)
+    
+    # i = 0
+    # already_liked = false
+    # p result.length
+    # while (already_liked == false) & (i < result.length)
+    #     puts result[i].to_i
+    #     p i
+    #     if result[i] == watch_id
+    #         already_liked = true
+    #     else
+    #         already_liked = false
+    #     end
+    #     i += 1
+    # end
+    # p already_liked
+    # if already_liked == true
+    #     "Du har redan gillat denna klockan!"
+    # else
+        # db.execute("INSERT INTO user_favourites (user_id, watch_id) VALUES (?,?)", user_id, watch_id)
+    # end
+    
+    db.execute("INSERT INTO user_favourites (user_id, watch_id) VALUES (?,?)", user_id, watch_id)
+    redirect('/watches/favourites')
 end
 
 get('/watches/:id') do
@@ -111,5 +143,5 @@ get('/watches/:id') do
     db = SQLite3::Database.new("db/watch_database.db")
     db.results_as_hash = true
     result = db.execute("SELECT * FROM watches WHERE watch_id = ?", id).first
-    slim(:"watches/show", locals:{result:result})
+    slim(:"/watches/show", locals:{result:result})
   end
