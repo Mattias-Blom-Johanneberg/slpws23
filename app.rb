6c1f76151cd59e7b27068166558ea1aa3bdb4da0
Mattias-Blom-Johanneberg <mattias.blom@elev.ga.ntig.se>
2023-05-08 21:47:40 +0200

    yardoc...

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require_relative './model.rb'
 
 enable :sessions
 
+include Model
+
 before('/*') do
     @user_id = session[:id]
 end
@@ -22,52 +24,68 @@ before('/watches/show') do
     @admin_check = (session[:id] == 1? true : false)
 end
 
-get('/error') do
-    error = session[:error]
-    slim(:error, locals:{error:error})
-end
-
-get('/')  do
-    db = SQLite3::Database.new('db/watch_database.db')
-    db.results_as_hash = true
-    
-    if (user_id = session[:id]) == nil
-        slim(:start)
-    else
-        result = db.execute("SELECT username FROM users WHERE user_id = ?", user_id)
-        slim(:user_start, locals:{user:result})
-    end
+# Displays the landing page
+#
+# @see Model#start
+get('/') do
+    user_id = session[:id]
+    start(user_id)
 end 
 
+# Displays a login form and a link to register account or, if user is logged in, redirects to '/logout'
+#
 get('/login') do
     if session[:id] != nil
-        slim(:logout)
+        redirect('/logout')
     else
         slim(:login)
     end
 end
 
+# Displays a logout form
+#
 get('/logout') do
-    session.destroy
-    redirect('/')
+    slim(:logout)
 end
 
+# Displays a register form
+#
 get('/register') do
     slim(:register)
 end
 
+# Attempts login and updates the session 
+#
+# @param [String] username, The username
+# @param [String] password, The password
+#
+# @see Model#login
 post('/login') do
     cooldown()
     username = params[:username]
     password = params[:password]
     
-    if user_login(username, password)
+    if login(username, password)
         redirect('/')
     else
         "Fel lösenord!"
     end
 end
 
+# User logs out and session is destroyed
+#
+post('/logout') do
+    session.destroy
+    redirect('/')
+end
+
+# Attempts login/register and updates the session
+#
+# @param [String] username, The username
+# @param [String] password, The password
+# @param [String] password_confirm, The repeated password
+#
+# @see Model#register_user
 post('/users/new') do
     username = params[:username]
     password = params[:password]
@@ -80,81 +98,130 @@ post('/users/new') do
     end
 end
 
+# Displays a list of all watches
+#
+# @see Model#show_all_watches
 get('/watches/show') do
     result = show_all_watches()
     slim(:"/watches/index", locals:{watches:result, admin:@admin_check})
 end
 
+# Displays a list of all watches liked by the user
+#
+# @see Model#show_favourite_watches
 get('/watches/favourites') do
-    user_id = session[:id]
-    result = show_favourite_watches(user_id)
+    # user_id = session[:id]
+    result = show_favourite_watches(@user_id)
     slim(:"/watches/favourites", locals:{favourites:result})
 end
 
+# Displays a form to create/add a watch
+#
 get('/watches/new') do
-        slim(:"/watches/new")
+    slim(:"/watches/new")
 end
 
+# Displays a single watch
+#
+# @param [Integer] :id, The ID of the watch
+#
+# @see Model#get_watch
+get('/watches/:id') do
+    id = params[:id].to_i
+    result = get_watch(id)
+    slim(:"/watches/show", locals:{result:result})
+end
+
+# Creates a new watch and redirects to '/watches/show'
+#
+# @param [String] watch_name, The name of the watch
+# @param [String] brand_name, The name of the brand
+# @param [String] content, The content of the watch
+# @param [String] movement, The movement of the watch
+# @param [Integer] :watch_id, The ID of the watch
+#
+# @see Model#create_new_watch
 post('/watches/new') do
     watch_name = params[:watch_name]
     brand_name = params[:brand_name]
     content = params[:content]
     movement = params[:movement]
     watch_id = params[:watch_id].to_i
-    user_id = session[:id]
-    register_new_watch(watch_name, brand_name, content, movement, watch_id, user_id)
+    user_id = session[:id].to_i
+    create_watch(watch_name, brand_name, content, movement, watch_id, user_id)
     redirect('/watches/show')
 end
 
+# Deletes an existing watch and redirects to '/watches/show'
+#
+# @param [Integer] :id, The ID of the user
+#
+# @see Model#delete_watch
 post('/watches/:id/delete') do
     id = params[:id].to_i
-    delete_a_watch(id)
+    delete_watch(id)
     redirect('/watches/show')
 end  
 
+# Updates an existing watch and redirects to '/watches/show'
+#
+# @param [Integer] :id, The ID of the watch
+# @param [String] watch_name, The name of the watch
+# @param [String] :brand_id, The ID of the brand
+#
+# @see Model#update_watch
 post('/watches/:id/update') do
     id = params[:id].to_i
     watch_name = params[:watch_name]
     brand_id = params[:brand_id].to_i
-    update_a_watch(id, watch_name, brand_id)
+    update_watch(id, watch_name, brand_id)
     redirect('/watches/show')
 end
 
+# Displays an edit form
+#
+# @param [Integer] :id, The ID of the watch
+#
+# @see Model#edit_watch
 get('/watches/:id/edit') do
     id = params[:id].to_i
-    result = edit_a_watch(id)    
+    result = edit_watch(id)    
     slim(:"/watches/edit", locals:{result:result})
 end
 
-get('/watches/:id/like') do
+# Attempts to adds watch to users favourites and redirects to '/watches/favourites'
+#
+#
+# @param [Integer] :id, The ID of the watch
+#
+# @see Model#like_watch
+post('/watches/:id/like') do
     watch_id = params[:id].to_i
     user_id = session[:id]
 
-    if like_a_watch(watch_id, user_id)
+    if like_watch(watch_id, user_id)
         "Du har redan gillat denna klockan!"
     else
         redirect('/watches/favourites')
     end
 end
 
-get('/watches/:id/dislike') do
+# Deletes watch from users favourites and redirects to '/watches/favourites'
+#
+# @param [Integer] :id, The ID of the watch
+#
+# @see Model#dislike_watch
+post('/watches/:id/dislike') do
     watch_id = params[:id].to_i
     user_id = session[:id]
-    dislike_a_watch(watch_id, user_id)
+    dislike_watch(watch_id, user_id)
     redirect('/watches/favourites')
 end
 
-get('/watches/favourites/:id/dislike') do
-    watch_id = params[:id].to_i
-    user_id = session[:id]
-    db = SQLite3::Database.new("db/watch_database.db")
-    db.results_as_hash = true
-    db.execute("DELETE FROM user_favourites WHERE user_id = ? AND watch_id = ?", user_id, watch_id)
-    redirect('/watches/favourites')
-end
 
-get('/watches/:id') do
-    id = params[:id].to_i
-    result = show_specific_watch(id)
-    slim(:"/watches/show", locals:{result:result})
+# Displays an error message
+#
+get('/error') do
+    error = session[:error]
+    slim(:error, locals:{error:error})
 end
--- model.rb
+++ model.rb
@@ -1,139 +1,185 @@
-def check_input(input)
-    if input == " "
-        session[:error] = "Input måste bestå av minst ett tecken annat än mellanslag"
-        redirect('/error')
+module Model
+
+    # Checks input and redirects to '/error' if input is wrong
+    #
+    # @param [String] input The input to check
+    #
+    def check_input(input)
+        if input == " "
+            session[:error] = "Input måste bestå av minst ett tecken annat än mellanslag"
+            redirect('/error')
+        end
     end
-end
 
-def cooldown()
-    timenow = Time.now
-    if session[:time] == nil
-      session[:time] = [timenow]
-    else
-      session[:time].prepend(timenow)
+    # Cooldown for login
+    #
+    def cooldown()
+        timenow = Time.now
+        if session[:time] == nil
+        session[:time] = [timenow]
+        else
+        session[:time].prepend(timenow)
+        end
+        timediff = timenow.to_i-session[:time][1].to_i
+        if timediff < 10 && session[:time].length > 1
+            sleep(2)
+        end
     end
-    timediff = timenow.to_i-session[:time][1].to_i
-    if timediff < 10 && session[:time].length > 1
-        sleep(2)
-    end
-end
 
-def user_login(username, password)
-    check_input(username)
-    check_input(password)
-
-    db = SQLite3::Database.new('db/watch_database.db')
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
-    
-    pwdigest = result["pwdigest"]
-    user_id = result["user_id"]
-
-    if BCrypt::Password.new(pwdigest) == password
-        session[:id] = user_id
-        return true
-    else
-        return false
-    end
-end
-
-def register_user(username, password, password_confirm)
-    
-    check_input(username)
-    check_input(password)
-
-    if (password == password_confirm)
-        password_digest = BCrypt::Password.create(password)
+    # Shows landing page 
+    #
+    # @param [Integer] user_id The ID of the user
+    #
+    def start(user_id)
         db = SQLite3::Database.new('db/watch_database.db')
-        db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)", username, password_digest)
-        return true
-    else 
+        db.results_as_hash = true
+        
+        if user_id == nil
+            slim(:start)
+        else
+            result = db.execute("SELECT username FROM users WHERE user_id = ?", user_id)
+            slim(:user_start, locals:{user:result})
+        end
+    end
+
+    # Attempts to login
+    #
+    # @param [String] username The username of the user
+    # @param [String] password The password of the user
+    #
+    # @see Model#check_input
+    def login(username, password)
+        check_input(username)
+        check_input(password)
+
+        db = SQLite3::Database.new('db/watch_database.db')
+        db.results_as_hash = true
+        result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+        
+        pwdigest = result["pwdigest"]
+        user_id = result["user_id"]
+
+        if BCrypt::Password.new(pwdigest) == password
+            session[:id] = user_id
+            return true
+        else
+            return false
+        end
+    end
+
+    # Attempts to register
+    #
+    # @param [String] username The chosen username 
+    # @param [String] password The chosen password 
+    # @param [String] password_confirm The repeated password 
+    #
+    # @see Model#check_input
+    def register_user(username, password, password_confirm)
+        
+        check_input(username)
+        check_input(password)
+
+        if (password == password_confirm)
+            password_digest = BCrypt::Password.create(password)
+            db = SQLite3::Database.new('db/watch_database.db')
+            db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)", username, password_digest)
+            return true
+        else 
+            return false
+        end
+    end
+
+    # Finds elements
+    #
+    # @return [Hash]
+    #   * watch_id [Integer] The IDs of all watches
+    #   * watch_name [String] The names of all watches
+    #   * user_id [Integer] The ID of the user owner of every watch
+    def show_all_watches()
+        db = SQLite3::Database.new('db/watch_database.db')
+        db.results_as_hash = true
+        result = db.execute("SELECT watch_id, watch_name, user_id FROM watches")
+        return result
+    end
+
+    def show_favourite_watches(user_id)
+        db = SQLite3::Database.new('db/watch_database.db')
+        db.results_as_hash = true
+        result = db.execute("SELECT * FROM user_favourites JOIN watches ON user_favourites.watch_id = watches.watch_id WHERE user_favourites.user_id = ?", user_id)
+        return result
+    end
+
+    def create_watch(watch_name, brand_name, content, movement, watch_id, user_id)
+        check_input(watch_name)
+        check_input(brand_name)
+        check_input(content)
+        check_input(movement)
+
+        db = SQLite3::Database.new("db/watch_database.db")
+        brand_id = db.execute("SELECT brand_id FROM watch_brands WHERE brand_name = ?", brand_name).first
+        db.execute("INSERT INTO watches (watch_name, brand_name, brand_id, content, movement, user_id) VALUES (?,?,?,?,?,?)", watch_name, brand_name, brand_id, content, movement, user_id)
+    end
+
+    def delete_watch(id)
+        db = SQLite3::Database.new("db/watch_database.db")
+        db.execute("DELETE FROM watches WHERE watch_id = ?", id)
+    end
+
+    def update_watch(id, watch_name, brand_id)
+        check_input(watch_name)
+
+        db = SQLite3::Database.new("db/watch_database.db")
+        db.execute("UPDATE watches SET watch_name = ?, brand_id = ? WHERE watch_id = ?", watch_name, brand_id, id)
+    end
+
+    def edit_watch(id)
+        db = SQLite3::Database.new("db/watch_database.db")
+        db.results_as_hash = true
+        return db.execute("SELECT * FROM watches WHERE watch_id = ?", id).first
+    end
+
+    def already_liked?(watch_id, result)
+        i = 0
+        while i < result.length
+            if watch_id == result[i][0]
+                return true
+            end
+        i += 1
+        end
         return false
     end
-end
 
-def show_all_watches()
-    db = SQLite3::Database.new('db/watch_database.db')
-    db.results_as_hash = true
-    result = db.execute("SELECT watch_id, watch_name, user_id FROM watches")
-end
-
-def show_favourite_watches(user_id)
-    db = SQLite3::Database.new('db/watch_database.db')
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM user_favourites JOIN watches ON user_favourites.watch_id = watches.watch_id WHERE user_favourites.user_id = ?", user_id)
-end
-
-def register_new_watch(watch_name, brand_name, content, movement, watch_id, user_id)
-    check_input(watch_name)
-    check_input(brand_name)
-    check_input(content)
-    check_input(movement)
-
-    db = SQLite3::Database.new("db/watch_database.db")
-    brand_id = db.execute("SELECT brand_id FROM watch_brands WHERE brand_name = ?", brand_name).first
-    db.execute("INSERT INTO watches (watch_name, brand_name, brand_id, content, movement, user_id) VALUES (?,?,?,?,?,?)", watch_name, brand_name, brand_id, content, movement, user_id)
-end
-
-def delete_a_watch(id)
-    db = SQLite3::Database.new("db/watch_database.db")
-    db.execute("DELETE FROM watches WHERE watch_id = ?", id)
-end
-
-def update_a_watch(id, watch_name, brand_id)
-    db = SQLite3::Database.new("db/watch_database.db")
-    db.execute("UPDATE watches SET watch_name = ?, brand_id = ? WHERE watch_id = ?", watch_name, brand_id, id)
-
-    check_input(watch_name)
-end
-
-def edit_a_watch(id)
-    db = SQLite3::Database.new("db/watch_database.db")
-    db.results_as_hash = true
-    return db.execute("SELECT * FROM watches WHERE watch_id = ?", id).first
-end
-
-def already_liked?(watch_id, result)
-    i = 0
-    while i < result.length
-        if watch_id == result[i][0]
-            return true
+    def like_watch(watch_id, user_id)
+        
+        db = SQLite3::Database.new("db/watch_database.db")
+        db.results_as_hash = true
+        result = db.execute("SELECT watch_id FROM user_favourites WHERE user_id = ?", user_id)
+        
+        i = 0
+        while i < result.length
+            if already_liked?(watch_id, result)
+                return true
+            end
+            i += 1
         end
-    i += 1
+        
+        db.execute("INSERT INTO user_favourites (user_id, watch_id) VALUES (?,?)", user_id, watch_id)
+        return false
     end
-    return false
-end
 
-def like_a_watch(watch_id, user_id)
-    puts watch_id, user_id
-    
-    db = SQLite3::Database.new("db/watch_database.db")
-    db.results_as_hash = true
-    result = db.execute("SELECT watch_id FROM user_favourites WHERE user_id = ?", user_id)
-    
-    i = 0
-    while i < result.length
-        if already_liked?(watch_id, result)
-            return true
-        end
-        i += 1
+    def dislike_watch(watch_id, user_id)
+        db = SQLite3::Database.new("db/watch_database.db")
+        db.results_as_hash = true
+        db.execute("DELETE FROM user_favourites WHERE user_id = ? AND watch_id = ?", user_id, watch_id)
     end
-    
-    db.execute("INSERT INTO user_favourites (user_id, watch_id) VALUES (?,?)", user_id, watch_id)
-    return false
-end
 
-def dislike_a_watch(watch_id, user_id)
-    db = SQLite3::Database.new("db/watch_database.db")
-    db.results_as_hash = true
-    db.execute("DELETE FROM user_favourites WHERE user_id = ? AND watch_id = ?", user_id, watch_id)
-end
+    def get_watch(id)
+        db = SQLite3::Database.new('db/watch_database.db')
+        db.results_as_hash = true
+        result = db.execute("SELECT * FROM watches WHERE watch_id = ?", id).first
+        owner = db.execute("SELECT username from users WHERE user_id = ?", result["user_id"])
+        result.store("username", owner[0][0])
+        return result
+    end
 
-def show_specific_watch(id)
-    db = SQLite3::Database.new("db/watch_database.db")
-    db.results_as_hash = true
-    result = db.execute("SELECT * FROM watches WHERE watch_id = ?", id).first
-    owner = db.execute("SELECT username from users WHERE user_id = ?", result['user_id'])
-    result.store("username", owner[0][0])
-    return result
 end
